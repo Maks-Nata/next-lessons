6d07d083021df36fbab7c1818d0082f96ab73b98
Maks-Nata <mixcim20121984@gmail.com>
2025-02-11 23:45:14 +0200

    see users

--- src/services/api.services.tsx
+++ src/services/api.services.tsx
@@ -1,15 +1,12 @@
 import {IUser} from "@/model/IUser";
 
-export const getAllUsers=async ():Promise<IUser[]>=>{
-    const users=await fetch('https://jsonplaceholder.typicode.com/users')
+export const getAllUsers = async (): Promise<IUser[]> => {
+    const users = await fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json()
-    )
+        )
     return users
 }
 export const getUserById = async (id: string): Promise<IUser> => {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
-    if (!response.ok) {
-        throw new Error(`Ошибка при получении пользователя с ID ${id}`);
-    }
-    return response.json();
+    const user = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`).then(response => response.json());
+return user
 };
