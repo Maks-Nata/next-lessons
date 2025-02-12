c6665d1b80f7d118fe5727b6527c5372742dde19
Maks-Nata <mixcim20121984@gmail.com>
2025-02-12 12:12:31 +0200

    see list users

--- src/components/UserComponent.tsx
+++ src/components/UserComponent.tsx
@@ -1,12 +1,13 @@
 
 import { IUser } from "@/model/IUser";
 import Link from "next/link";
+import {FC} from "react";
 
 interface UserComponentProps {
     user: IUser;
 }
 
-const UserComponent: React.FC<UserComponentProps> = ({ user }) => (
+const UserComponent: FC<UserComponentProps> = ({ user }) => (
     <div>
         <h2>Пользователь: {user.name}</h2>
         <p>ID: {user.id}</p>
